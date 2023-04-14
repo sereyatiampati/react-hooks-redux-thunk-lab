709bec16861fe0ac30cb7b92c7b5f0fd9bf79370
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-15 00:50:54 +0300

    edited the display of the cats

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -4,7 +4,12 @@
         <div className="cats" style={{display: "flex", flexWrap: "wrap"}}>
        { catPics.map( cat => {
        return (
-        <img src={cat.url} className="card-img-top" alt='cat'key={cat.id}/>
+    <div className="card" style={{width: "18rem", margin: '2rem'}} key={cat.id}>
+        <img src={cat.url} className="card-img-top" alt='cat'/>
+        <div className="card-body">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    </div> 
        );
        }
        )
@@ -14,10 +19,3 @@
 }
  
 export default CatList;
-
-{/* <div className="card" style={{width: "18rem", margin: '2rem'}} key={cat.id}>
-            <img src={cat.url} class="card-img-top" alt={cat.url}/>
-            <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div> */}
